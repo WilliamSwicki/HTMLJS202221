e66f745645d96b06605a9dc1b0fc68ef452e1981
William Swicki <wmswicki@email.neit.edu>
2023-04-05 10:30:30 -0400

    ball changes collor and speed up

--- gds137/Week 1/js/week1_basic_movement.js
+++ gds137/Week 1/js/week1_basic_movement.js
@@ -3,6 +3,7 @@
 var canvas;
 var context;
 var timer;
+var accelration = 1.05;
 //1000 ms or 1 second / FPS
 var interval = 1000/60;
 var ball;
@@ -29,24 +30,36 @@ function animate()
 
 	if(ball.x+ball.width/2>canvas.width)
 	{
-		ball.vx = -ball.vx;
+		ball.vx = -ball.vx*accelration;
+		ball.color="#00ff00";
+		if (ball.vx >10 ||ball.vx<-10)
+		{
+			ball.vx=-5;
+		}
 	}
 	
 	if(ball.x<ball.width/2)
 	{
-		ball.vx = -ball.vx;
+		ball.vx = -ball.vx*accelration;
+		ball.color="#ff0000";
 	}
 
 	ball.y += ball.vy;
 
 	if(ball.y+ball.height/2>canvas.height)
 	{
-		ball.vy = -ball.vy;
+		ball.vy = -ball.vy*accelration;
+		ball.color="#0000ff";
+		if (ball.vy >10 ||ball.vy<-10)
+		{
+			ball.vy=-5;
+		}
 	}
 	
 	if(ball.y<ball.height/2)
 	{
-		ball.vy = -ball.vy;
+		ball.vy = -ball.vy*accelration;
+		ball.color="#ffff00";
 	}
 
 	//Update the Screen
